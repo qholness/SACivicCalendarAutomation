3c01ffe53274ba310ce424f7066b3edfc31529ec
Quentin Holness <qholness@gmail.com>
2019-10-27 09:48:02 -0500

    Catch HttpErrors This commit fixes an error between the DB and the calendar where a DB's calendar ID gets stale and can't be found in the calendar. We catch the error and allow it to pass, removing it from the DB.

--- common/__init__.py
+++ common/__init__.py
@@ -2,6 +2,7 @@ from sqlalchemy.engine import RowProxy, Connection
 from gcal.CalendarOps  import CalendarOps
 from tqdm import tqdm
 from dba import delete_old_events
+from googleapiclient.errors import HttpError
 
 
 def create_new_events(tgt: str, events: RowProxy, ops: CalendarOps, con: Connection, cfg=None):
@@ -11,5 +12,8 @@ def create_new_events(tgt: str, events: RowProxy, ops: CalendarOps, con: Connect
 
 def remove_old_events(tgt: str, events: RowProxy, ops: CalendarOps, con: Connection):
     for event in tqdm(events, desc="Removing old events"):
-        ops.delete_event(tgt, event.calendarId)
+        try:
+            ops.delete_event(tgt, event.calendarId)
+        except HttpError as e:
+            print(e)
     delete_old_events(con)
